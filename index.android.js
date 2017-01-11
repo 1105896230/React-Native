93f748ae22cdc83137eb8ecb4c978b0a1dc8de24
fmt <1105896230@qq.coM>
2017-01-12 00:20:10 +0800

    listView

--- index.android.js
+++ index.android.js
@@ -9,34 +9,126 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    TextInput,
+    ScrollView,
+    Image,
+    ListView
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
+            <View>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+                <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+}
+class FixedDimensionsBasics extends Component {
+    render() {
+        return (
+            <View>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+                <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+class FlexDimensionsBasics extends Component {
+    render() {
+        return (
+            // 试试去掉父View中的`flex: 1`。
+            // 则父View不再具有尺寸，因此子组件也无法再撑开。
+            // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+class FlexDirectionBasics extends Component {
+    render() {
+        return (
+            // 尝试把`flexDirection`改为`column`看看
+            <View style={{flex: 1, flexDirection: 'column'}}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+class JustifyContentBasics extends Component {
+    render() {
+        return (
+            // 尝试把`justifyContent`改为`center`看看
+            // 尝试把`flexDirection`改为`row`看看
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+class AlignItemsBasics extends Component {
+    render() {
+        return (
+            // 尝试把`alignItems`改为`flex-start`看看
+            // 尝试把`justifyContent`改为`flex-end`看看
+            // 尝试把`flexDirection`改为`row`看看
+            <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
+class PizzaTranslator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    render() {
+        return (
+            <View style={{padding: 10}}>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
                 </Text>
             </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        width:400,
+        height:200,
     },
     welcome: {
         fontSize: 20,
@@ -49,5 +141,51 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
 });
+class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return(
+            <ScrollView>
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+                <Text style={{fontSize:96}}>Scroll me plz</Text>
+                <Image source={pic} style={{width: 193, height: 110}} />
+                <Text style={{fontSize:96}}>If you like</Text>
+
+                <Text style={{fontSize:96}}>Scrolling down</Text>
+
+                <Text style={{fontSize:96}}>What's the best</Text>
+
+                <Text style={{fontSize:96}}>Framework around?</Text>
+
+                <Text style={{fontSize:80}}>React Native</Text>
+            </ScrollView>
+        );
+    }
+}
+
+class ListViewBasics extends Component {
+    // 初始化模拟数据
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+            ])
+        };
+    }
+    render() {
+        return (
+            <View style={{flex: 1, paddingTop: 22}}>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Text>{rowData}</Text>}
+                />
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => ListViewBasics);
