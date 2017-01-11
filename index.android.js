aba213007098679ad0f78af661ac7b01e86e1331
fmt <1105896230@qq.coM>
2017-01-11 23:52:12 +0800

    修改

--- index.android.js
+++ index.android.js
@@ -5,44 +5,49 @@
  */
 
 import React, { Component } from 'react';
-
-
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
-  render() {
-    let pic = {
-      uri: 'https://ss0.baidu.com/73t1bjeh1BF3odCf/it/u=3451371770,2899345145&fm=85&s=BB887023199DC0CE10DCC9DA0300C0B1'
-    };
-    return (
-      <Image source={pic} style={{width: 193, height: 110}} />
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
