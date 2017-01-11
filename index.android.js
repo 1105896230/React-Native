7f9eb4405ba65076d1f56eaf51a1f387e4a64289
fmt <1105896230@qq.coM>
2017-01-12 00:32:14 +0800

    界面跳转

--- index.android.js
+++ index.android.js
@@ -13,19 +13,36 @@ import {
     TextInput,
     ScrollView,
     Image,
-    ListView
+    ListView,
+    Navigator
 } from 'react-native';
-
+import MyScene from './MyScene';
 export default class AwesomeProject extends Component {
+    static defaultProps = {
+        title: 'MyScene'
+    };
+
     render() {
         return (
             <View>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <Text style={{width: 50, height: 50, backgroundColor: 'powderblue'}} >
+                    Hi! My name is {this.props.title}.
+                </Text>
                 <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
                 <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
             </View>
         );
     }
+    // render() {
+    //     return (
+    //         <Navigator
+    //             initialRoute={{ title: 'My Initial Scene', index: 0 }}
+    //             renderScene={(route, navigator) => {
+    //                 return <MyScene title={route.title} />
+    //             }}
+    //         />
+    //     );
+    // }
 }
 class FixedDimensionsBasics extends Component {
     render() {
@@ -175,7 +192,9 @@ class ListViewBasics extends Component {
                 'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
             ])
         };
+
     }
+
     render() {
         return (
             <View style={{flex: 1, paddingTop: 22}}>
@@ -187,5 +206,35 @@ class ListViewBasics extends Component {
         );
     }
 }
+class SimpleNavigationApp extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ title: 'My Initial Scene', index: 0 }}
+                renderScene={(route, navigator) =>
+                    <MyScene
+                        title={route.title}
 
-AppRegistry.registerComponent('AwesomeProject', () => ListViewBasics);
+                        // Function to call when a new scene should be displayed
+                        onForward={ () => {
+                            const nextIndex = route.index + 1;
+                            navigator.push({
+                                title: 'Scene ' + nextIndex,
+                                index: nextIndex,
+                            });
+                        }}
+
+                        // Function to call to go back to the previous scene
+                        onBack={() => {
+                            if (route.index > 0) {
+                                navigator.pop();
+                            }
+                        }}
+                    />
+                }
+            />
+        )
+    }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => SimpleNavigationApp);
