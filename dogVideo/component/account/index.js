55e7a90a8c9d16d5fe45925dfe0d932464f31cfa
fmt <1105896230@qq.com>
2017-10-23 16:39:28 +0800

    编辑

--- dogVideo/component/account/index.js
+++ dogVideo/component/account/index.js
@@ -11,6 +11,7 @@ import {
   Alert,
   TouchableOpacity,
   Dimensions,
+  Modal,
 } from 'react-native';
 import request from '../common/netUtils'
 import config from '../common/config'
@@ -21,26 +22,28 @@ export default class Account extends Component {
 
   constructor(props) {
     super(props)
-    var u=this.props.user
-  
+    var u = this.props.user
+
     this.state = ({
-      user: u
+      user: u,
+      modalvisable: false,
+
     })
   }
 
   _pickPhoto() {
-    var that=this
+    var that = this
     var options = {
       title: '选择头像',
-      cancelButtonTitle:'取消',
-      takePhotoButtonTitle:'拍照',
-      chooseFromLibraryButtonTitle:'选择相册',
-      quality:0.75,
-      allowsEditing:true,
-      noData:false,
-      storageOptions:{
-        skipBackup:true,
-        path:'images'
+      cancelButtonTitle: '取消',
+      takePhotoButtonTitle: '拍照',
+      chooseFromLibraryButtonTitle: '选择相册',
+      quality: 0.75,
+      allowsEditing: true,
+      noData: false,
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
       }
     };
     ImagePicker.showImagePicker(options, (response) => {
@@ -63,6 +66,11 @@ export default class Account extends Component {
       <View style={styles.container}>
         <View style={styles.toolbar}>
           <Text style={styles.toolbarText}>我的账号</Text>
+          <Text style={styles.toolBarEdit} onPress={() => {
+            this.setState({
+              modalvisable: true
+            })
+          }}>编辑</Text>
         </View>
 
         {
@@ -73,6 +81,7 @@ export default class Account extends Component {
                   <Image
                     source={{ uri: this.props.user.avater }}
                     style={styles.avater}
+
                   />
                 </View>
                 <Text style={styles.avaterTip}>
@@ -84,7 +93,7 @@ export default class Account extends Component {
               <Text style={styles.avaterTip}>
                 添加头像
               </Text>
-              <TouchableOpacity style={styles.avaterBox}  onPress={this._pickPhoto.bind(this)}>
+              <TouchableOpacity style={styles.avaterBox} onPress={this._pickPhoto.bind(this)}>
                 <Image
                   source={require("../source/ic_launcher.png")}
                   style={styles.icon}
@@ -93,8 +102,47 @@ export default class Account extends Component {
             </View>
         }
 
+        {this.state.modalvisable ? <Modal
+          animated={true}
+          visible={this.state.modalvisable}>
+          <View style={styles.modalContainer}>
+            <Image
+              source={require("../source/ic_launcher.png")}
+              style={styles.closeIcon}
+              onPress={() => {
+                this.setState({
+                  modalvisable: false
+                })
+              }}
+            />
+            <View style={styles.filedItem}>
+              <Text style={styles.lable}>昵称</Text>
+              <TextInput
+                placeholder="输入你的昵称"
+                style={styles.inputFiled}
+                autoCapitalize={'none'}
+                autoCorrect={false}
+                defaultValue={this.state.user.nickName}
+                onChangeText={(text) => {
+                  this.changeUserText('nickName', text)
+                }} />
+
+            </View>
+
+          </View>
+
+
+        </Modal>:<View/>}
+
       </View>
     )
+  }
+  changeUserText(key,value){
+    var user=this.state.user
+    user[key]=value
+    this.setState({
+      user:user
+    })
   }
 
 }
@@ -149,5 +197,49 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: width * 0.1
+  },
+  toolBarEdit: {
+    position: 'absolute',
+    right: 10,
+    top: 26,
+    color: '#fff',
+    textAlign: 'right',
+    fontWeight: '600',
+    fontSize: 14
+  },
+  modalContainer: {
+    flex: 1,
+    paddingTop: 50,
+    backgroundColor: '#fff',
+
+  },
+  filedItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderColor: '#eee',
+    borderBottomWidth: 1,
+  },
+  lable: {
+    color: '#eee',
+    marginRight: 10,
+  },
+  inputFiled: {
+    height: 50,
+    flex: 1,
+    color: '#666',
+    fontSize: 14
+  },
+  closeIcon: {
+    position: 'absolute',
+    width: 40,
+    height: 40,
+    fontSize: 32,
+    right: 20,
+    top: 30,
+
   }
 })
